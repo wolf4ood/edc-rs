01114327429a5f1b722924307ad43d1af6775c41
Enrico Risa <enrico.risa@gmail.com>
2024-09-10 18:10:23 +0200

    feat: connector docker publish

--- testing/connector/build.gradle.kts
+++ testing/connector/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+import com.github.jengelman.gradle.plugins.shadow.ShadowJavaPlugin
+
 plugins {
     `java-library`
     id("application")
     alias(libs.plugins.shadow)
+    alias(libs.plugins.docker)
 }
 
 repositories {
@@ -46,3 +50,20 @@ tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
     mergeServiceFiles()
     archiveFileName.set("connector.jar")
 }
+
+//actually apply the plugin to the (sub-)project
+apply(plugin = "com.bmuschko.docker-remote-api")
+// configure the "dockerize" task
+val dockerTask: DockerBuildImage = tasks.create("dockerize", DockerBuildImage::class) {
+    val dockerContextDir = project.projectDir
+    dockerFile.set(file("$dockerContextDir/src/main/docker/Dockerfile"))
+    images.add("${project.name}:${project.version}")
+    images.add("${project.name}:latest")
+    // specify platform with the -Dplatform flag:
+    if (System.getProperty("platform") != null)
+        platform.set(System.getProperty("platform"))
+    buildArgs.put("JAR", "build/libs/${project.name}.jar")
+    inputDir.set(file(dockerContextDir))
+}
+// make sure  always runs after "dockerize" and after "copyOtel"
+dockerTask.dependsOn(tasks.named(ShadowJavaPlugin.SHADOW_JAR_TASK_NAME))
