f262d1b449ba3c47b781354be00af001e2702759
wolf4ood <enrico.risa@gmail.com>
2024-10-01 00:05:03 +0200

    chore: add missing clone on EDR entry

--- edc-connector-client/src/types/edr.rs
+++ edc-connector-client/src/types/edr.rs
@@ -1,6 +1,6 @@
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct EndpointDataReferenceEntry {
     asset_id: String,
